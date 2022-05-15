390acf175110c3051b9a918bbc57f8c0521d651c
fibb-bs_Git <jhonatanalvarez99@outlook.com>
2022-05-15 04:51:48 -0400

    Lectura hasta posicionamiento en estructura de daatos

--- Codigo/src/App.java
+++ Codigo/src/App.java
@@ -12,10 +12,31 @@ public class App {
         try {
             Scanner scan = new Scanner(new File("Codigo/entradas/01.txt"));
             String[] medidas = scan.nextLine().split(",");
-            Terreno [][] superficie = 
-            while(scan.hasNext()){
-                String linea = scan.nextLine();
-            }  
+            int filas = Integer.parseInt(medidas[0]);
+            int columnas = Integer.parseInt(medidas[1]);
+            Terreno [][] superficie = new Terreno[filas][columnas];
+            for (int i = 0; i < filas; i++) {
+                int fila = Integer.parseInt(scan.nextLine());
+                for (int j = 0; j < columnas; j++) {
+                    String[] linea = scan.nextLine().split(",");
+                    int columna = Integer.parseInt(linea[0]);
+                    String tipo = linea[1];
+                    Terreno t;
+                    if (tipo.equals("L")){
+                        t = new TerrenoLlano(fila, columna);
+                    }
+                    else{
+                        t = new TerrenoAbrupto(fila, columna);
+                    }
+                    try {
+                        String objetivo = linea[2];
+                        t.setObjetivo();
+                        superficie[fila][columna] = t;
+                    } catch (Exception e) {
+                        superficie[fila][columna] = t;
+                    }
+                }
+            } 
             
         } catch (Exception e) {
             System.out.println(e.getMessage());
