84b6b404c9ea303cd2f9bf9a10fea228f3384101
fibb-bs_Git <jhonatanalvarez99@outlook.com>
2022-05-16 22:10:15 -0400

    Replanteamiento de estructura de datos (2)

--- Codigo/src/App.java
+++ Codigo/src/App.java
@@ -30,33 +30,43 @@ public class App {
                         terrenoIJ = new TerrenoAbrupto(i, j);
                         superficie.add(terrenoIJ);
                     }
-                    for (int k = 0; k < movimientoI.length; k++) {
-                        int coordIHijo = i+movimientoI[k];
-                        int coordJHijo = j+movimientoJ[k];
-                        if (superficie.checkTerreno(coordIHijo, coordJHijo)){
-                            Terreno terrenoHijo;
-                            if (!superficie.exists(coordIHijo, coordJHijo)){
-                                double randHijo = Math.random();
-                                if (randHijo<=0.7){
-                                    terrenoHijo = new TerrenoLlano(coordIHijo, coordJHijo);
-                                }
-                                else{
-                                    terrenoHijo = new TerrenoAbrupto(coordIHijo, coordJHijo);
-                                }
-                            }
-                            else{
-                                terrenoHijo = superficie.find(i, j);
-                            }
-                            terrenoIJ.getTerrenos().add(terrenoHijo);
-                            superficie.add(terrenoHijo);
-                        }
-                        else{
-                            //Aquí no va nada ya que las coordenadas hijo se salen de la superficie.
-                        }
-                    }
                 }
                 else{
+                    //Si el terreno de coordenadas i,j ya existe, solo se obtiene desde la superficie.
                     terrenoIJ = superficie.find(i, j);
+                }
+                /**
+                 * Al terreno recién creado (u obtenido desde la superficie) se le asignan máximo 4 hijos.
+                 * Estos hijos son los nodos los cuales serán las opciones a abrir.
+                 * Si el nodo hijo ya fue creado, no es necesario volver a crearlo.
+                 * Se emplea la misma lógica anterior 
+                 */
+                for (int k = 0; k < movimientoI.length; k++) {
+                    //Los cuatro hijos están definidos según una serie de coordenadas posibles.
+                    int coordIHijo = i+movimientoI[k];
+                    int coordJHijo = j+movimientoJ[k];
+                    //Se debe checkear que el terreno hijo a crear o a obtener pueda existir dentro de la superficie.
+                    if (superficie.checkTerreno(coordIHijo, coordJHijo)){
+                        Terreno terrenoHijo;
+                        //Se debe checkear que el terreno ya exista o no dentro de la superficie.
+                        if (!superficie.exists(coordIHijo, coordJHijo)){
+                            double randHijo = Math.random();
+                            if (randHijo<=0.7){
+                                terrenoHijo = new TerrenoLlano(coordIHijo, coordJHijo);
+                            }
+                            else{
+                                terrenoHijo = new TerrenoAbrupto(coordIHijo, coordJHijo);
+                            }
+                        }
+                        else{
+                            terrenoHijo = superficie.find(i, j);
+                        }
+                        terrenoIJ.getTerrenos().add(terrenoHijo);
+                        superficie.add(terrenoHijo);
+                    }
+                    else{
+                        //Aquí no va nada ya que las coordenadas hijo se salen de la superficie.
+                    }
                 }
             }
         }
