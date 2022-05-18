b0a905098791ac25e80a276091808b76e3f7b5b8
fibb-bs_Git <jhonatanalvarez99@outlook.com>
2022-05-18 00:33:47 -0400

    Creando objetivo

--- Codigo/src/App.java
+++ Codigo/src/App.java
@@ -6,7 +6,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import java.awt.image.BufferedImage;
-import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -21,105 +22,8 @@ public class App {
         System.out.print("Ingrese dimensión m: ");
         int m = Integer.parseInt(scan.nextLine());
         System.out.println("Dimensión N:"+n+", Dimensión M:"+m);
-        JFrame frame = new JFrame();
-        //
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600,600);
-        frame.setLayout(new GridLayout(n,m,1,1));
-        //
         Terrenos superficie = new Terrenos(n,m);
-        int[] movimientoI = new int[]{0,1,0,-1};
-        int[] movimientoJ = new int[]{1,0,-1,0};
-        for (int i = 0; i < n; i++) {//filas
-            for (int j = 0; j < m; j++) {//columnas
-                Terreno terrenoIJ;
-                if (!superficie.exists(i, j)){
-                    //Si el terreno de coordenadas i,j no existe, se crea (llano o abrupto en un ratio de 0.7 para llano).
-                    double rand = Math.random();
-                    if (rand<=0.7){
-                        //Terreno llano
-                        terrenoIJ = new TerrenoLlano(i, j);
-                        superficie.add(terrenoIJ);
-                    }
-                    else{
-                        //Terreno abrupto
-                        terrenoIJ = new TerrenoAbrupto(i, j);
-                        superficie.add(terrenoIJ);
-                    }
-                }
-                else{
-                    //Si el terreno de coordenadas i,j ya existe, solo se obtiene desde la superficie.
-                    terrenoIJ = superficie.find(i, j);
-                }
-                frame.add(terrenoIJ.getGrafico());
-                if (terrenoIJ instanceof TerrenoAbrupto){
-                    escritura.println("Abrupto ("+i+","+j+")");
-                }
-                else{
-                    escritura.println("Llano ("+i+","+j+")");
-                }
-                /**
-                 * Al terreno recién creado (u obtenido desde la superficie) se le asignan máximo 4 hijos.
-                 * Estos hijos son los nodos los cuales serán las opciones a abrir.
-                 * Si el nodo hijo ya fue creado, no es necesario volver a crearlo.
-                 * Se emplea la misma lógica anterior 
-                 */
-                escritura.println("\tTerrenos adyacentes:");
-                for (int k = 0; k < movimientoI.length; k++) {
-                    //Los cuatro hijos están definidos según una serie de coordenadas posibles.
-                    int coordIHijo = i+movimientoI[k];
-                    int coordJHijo = j+movimientoJ[k];
-                    //Se debe checkear que el terreno hijo a crear o a obtener pueda existir dentro de la superficie.
-                    if (superficie.checkTerreno(coordIHijo, coordJHijo)){
-
-                        Terreno terrenoHijo;
-                        //Se debe checkear que el terreno ya exista o no dentro de la superficie.
-                        if (!superficie.exists(coordIHijo, coordJHijo)){
-                            double randHijo = Math.random();
-                            if (randHijo<=0.7){
-                                terrenoHijo = new TerrenoLlano(coordIHijo, coordJHijo);
-                            }
-                            else{
-                                terrenoHijo = new TerrenoAbrupto(coordIHijo, coordJHijo);
-                            }
-                            if (Math.random()<=0.3){
-                                if(!terrenoIJ.getObstaculos().exists(coordIHijo, coordJHijo)){
-                                    terrenoIJ.getObstaculos().add(terrenoHijo);
-                                    //Código de bordes
-                                    terrenoIJ.pintarObstaculo();
-                                }
-                                terrenoHijo.getObstaculos().add(terrenoIJ);
-                                terrenoHijo.pintarObstaculo();
-                            }
-                            superficie.add(terrenoHijo);
-                        }
-                        else{
-                            terrenoHijo = superficie.find(coordIHijo, coordJHijo);
-                        }
-                        
-                        
-                        terrenoIJ.getTerrenos().add(terrenoHijo);
-                        
-                        if (terrenoHijo instanceof TerrenoAbrupto){
-                            escritura.println("\t\t"+terrenoIJ.getOrientacionRelativa(terrenoHijo)[0]+": Abrupto ("+coordIHijo+","+coordJHijo+")" + " Movimiento numero: " + (k+1) + " Obstaculo: " + (terrenoIJ.getObstaculos().exists(coordIHijo, coordJHijo)));
-                        }
-                        else{
-                            escritura.println("\t\t"+terrenoIJ.getOrientacionRelativa(terrenoHijo)[0]+": Llano ("+coordIHijo+","+coordJHijo+")" + " Movimiento numero: " + (k+1) + " Obstaculo: " + (terrenoIJ.getObstaculos().exists(coordIHijo, coordJHijo)));
-                        }
-                    }
-                    else{
-                        //Aquí no va nada ya que las coordenadas hijo se salen de la superficie.
-                    }
-                }
-
-            }
-            frame.setVisible(true);
-        }
-
-
-        
-        file.close();
-        takePicture(frame);
+        takePicture(superficie.getFrame());
     }
 
 
@@ -136,9 +40,11 @@ public class App {
                 panel.paint(screenshotImage.getGraphics()
         );
         try {
-            ImageIO.write(screenshotImage, "png", new File("screenShot.png" ));
+            DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("dd-MM-yy_HHmm");
+            String nowFormat = dtf2.format(LocalDateTime.now());
+            ImageIO.write(screenshotImage, "png", new File("Codigo/salidas/"+nowFormat+".png" ));
         } catch (IOException ex) {
-            System.err.println("ImageIsuues");
+            System.err.println("ImageIssues");
         }
     }
     
--- Codigo/src/Terrenos.java
+++ Codigo/src/Terrenos.java
@@ -1,11 +1,17 @@
 package src;
 
 import java.util.ArrayList;
+import java.awt.*;
+import javax.swing.JFrame;
 
 public class Terrenos {
+
+    private JFrame frame;
     private ArrayList<Terreno> terrenos;
-    int n;
-    int m;
+    private int n;
+    private int m;
+    private int[] movimientoI = new int[]{0,1,0,-1};
+    private int[] movimientoJ = new int[]{1,0,-1,0};
 
     public Terrenos() {
         this.terrenos = new ArrayList<>();
@@ -15,9 +21,92 @@ public class Terrenos {
         this.n = n;
         this.m = m;
         this.terrenos = new ArrayList<>();
-        //Se genera el terreno
-        //Lógica de creación random de terrenos abruptos y llanos con obstáculos y esas cosas.
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,600);
+        frame.setLayout(new GridLayout(n,m,1,1));
+        for (int i = 0; i < n; i++) {//filas
+            for (int j = 0; j < m; j++) {//columnas
+                Terreno terrenoIJ;
+                if (!this.exists(i, j)){
+                    //Si el terreno de coordenadas i,j no existe, se crea (llano o abrupto en un ratio de 0.7 para llano).
+                    double rand = Math.random();
+                    if (rand<=0.7){
+                        //Terreno llano
+                        terrenoIJ = new TerrenoLlano(i, j);
+                        this.add(terrenoIJ);
+                    }
+                    else{
+                        //Terreno abrupto
+                        terrenoIJ = new TerrenoAbrupto(i, j);
+                        this.add(terrenoIJ);
+                    }
+                }
+                else{
+                    //Si el terreno de coordenadas i,j ya existe, solo se obtiene desde la superficie.
+                    terrenoIJ = this.find(i, j);
+                }
+                frame.add(terrenoIJ.getGrafico());
+                /**
+                 * Al terreno recién creado (u obtenido desde la superficie) se le asignan máximo 4 hijos.
+                 * Estos hijos son los nodos los cuales serán las opciones a abrir.
+                 * Si el nodo hijo ya fue creado, no es necesario volver a crearlo.
+                 * Se emplea la misma lógica anterior 
+                 */
+                for (int k = 0; k < movimientoI.length; k++) {
+                    //Los cuatro hijos están definidos según una serie de coordenadas posibles.
+                    int coordIHijo = i+movimientoI[k];
+                    int coordJHijo = j+movimientoJ[k];
+                    //Se debe checkear que el terreno hijo a crear o a obtener pueda existir dentro de la superficie.
+                    if (this.checkTerreno(coordIHijo, coordJHijo)){
+
+                        Terreno terrenoHijo;
+                        //Se debe checkear que el terreno ya exista o no dentro de la superficie.
+                        if (!this.exists(coordIHijo, coordJHijo)){
+                            double randHijo = Math.random();
+                            if (randHijo<=0.7){
+                                terrenoHijo = new TerrenoLlano(coordIHijo, coordJHijo);
+                            }
+                            else{
+                                terrenoHijo = new TerrenoAbrupto(coordIHijo, coordJHijo);
+                            }
+                            
+                            this.add(terrenoHijo);
+                        }
+                        else{
+                            terrenoHijo = this.find(coordIHijo, coordJHijo);
+                        }
+                        
+                        if (Math.random()<=0.1){
+                            if(!terrenoIJ.getObstaculos().exists(coordIHijo, coordJHijo)){
+                                terrenoIJ.getObstaculos().add(terrenoHijo);
+                                //Código de bordes
+                                terrenoIJ.pintarObstaculo();
+                            }
+                            terrenoHijo.getObstaculos().add(terrenoIJ);
+                            terrenoHijo.pintarObstaculo();
+                        }
+
+                        terrenoIJ.getTerrenos().add(terrenoHijo);
+                    }
+                    else{
+                        //Aquí no va nada ya que las coordenadas hijo se salen de la superficie.
+                    }
+                }
+
+            }
+            frame.setVisible(true);
+            /**
+             * Creación de objetivo
+             * Para que sea coherente, el código solo posiciona al objetivo en el cuartel inferior derecho
+             */
+            int filaObjetivo = (int)(n/2) + (int)(Math.random() * ((n - (int)(n/2)) + 1));
+            int columnaObjetivo = (int)(m/2) + (int)(Math.random() * ((m - (int)(m/2)) + 1));
+            this.find(filaObjetivo, columnaObjetivo).setObjetivo();
+        }
+        
     }
+
     
     public ArrayList<Terreno> getTerrenos(){
         return terrenos;
@@ -65,5 +154,9 @@ public class Terrenos {
             return true;
         }
         return false;
+    }
+
+    public JFrame getFrame(){
+        return frame;
     }
 }
